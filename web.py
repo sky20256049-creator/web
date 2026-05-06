cb4f33aa4fe9eaae98f86113d10a6b39434abc1e
sky20256049-creator <sky.20256049@student.atmi.ac.id>
2026-05-06 14:21:52 +0700

    Update web.py

--- web.py
+++ web.py
@@ -234,11 +234,12 @@ if opsi == "Sakit":
             <p style="font-size:20px; color:black;">
                 Prosedur:
             </p>
-        1. Lapor pada hari H kepada instruktur/dosen yang mengampu pembelajaran(melalui wa).\n
+        1. Lapor pada hari H kepada kepala bengkel yang mengampu pembelajaran(melalui wa).\n
         2. Ketika masuk siapkan:\n
            -<b> Surat izin tidak masuk (boleh menyusul)\n
            -<b> Surat dari orang tua/wali (dengan tanda tangan)\n
-        3. lapor kepala bengkel sesuai prodi.\n
+        3. Minta tanda tangan kepala bengkel.\n
+        4. Kumpulkan semua surat kepada penghitung jam plus-minus
         </b>
         </div>
         """, unsafe_allow_html=True)
@@ -262,12 +263,13 @@ if opsi == "Sakit":
             <p style="font-size:20px; color:black;">
                 Prosedur:
             </p>
-         1. Lapor pada hari H kepada instruktur/dosen yang mengampu pembelajaran(melalui wa).\n
+         1. Lapor pada hari H kepada kepala bengkel yang mengampu pembelajaran(melalui wa).\n
          2. Ketika masuk siapkan:\n
            -<b> Surat dokter (rumah sakit / puskesmas)\n
            -<b> Surat izin tidak masuk\n
            -<b> Surat dari orang tua/wali *(dengan tanda tangan)*\n
-         3. lapor kepala bengkel sesuai prodi.\n
+         3. Minta tanda tangan kepala bengkel.\n
+         4. Kumpulkan semua surat kepada penghitung jam plus-minus
         </b>
         </div>
         """, unsafe_allow_html=True)
